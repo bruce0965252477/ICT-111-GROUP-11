513adf947d3197b4706330a326b0cf2c6bb160ea
bruce0965252477 <munanyangabruce13@gmail.com>
2025-11-07 00:13:24 +0200

    good messege

---
+++ cal.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+import math
+
+# -----------------------------
+# Global variables
+# ----------------------------
+last_result = None
+memory = 0.0
+history = []
+use_degrees = True  # Toggle between degrees and radians for trig
+
+# -----------------------------
+# Core Functions
