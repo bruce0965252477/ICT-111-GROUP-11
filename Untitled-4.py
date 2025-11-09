0a4896536f3290283ff7f5d82e10c84dd2eddb1a
aquirina <aquirinamulauzi@gmail.com>
2025-11-10 00:37:26 +0200

    good message

---
+++ Untitled-4.py
@@ -0,0 +1,13 @@
+# -----------------------------
+# Button Functions
+# -----------------------------
+def click(char):
+    entry.insert(tk.END, str(char))
+
+def clear():
+    entry.delete(0, tk.END)
+
+def backspace():
+    current = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(0, current[:-1])
