8d739b01e478411b328d8ea0b587f919a6d2e006
Taonga <patiencechishimba26@gmail.com>
2025-11-11 16:06:42 +0200

    goood message

---
+++ untitled-11.py
@@ -0,0 +1,3 @@
+# Degrees/Radians toggle
+degree_button = tk.Button(root,text="Degrees",width=10,height=2,font=("Arial",16),bg="#64b5f6",command=toggle_degrees)
+degree_button.grid(row=7,column=4,columnspan=2,padx=5,pady=5)
